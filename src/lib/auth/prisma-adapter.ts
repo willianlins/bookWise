ea6cbb93c2ae6bdcedcc71945528d3d4dbb5b8ae
Willian Lins Araújo <williannlins@gmail.com>
2024-06-26 07:43:05 -0300

    prisma adapter config

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -1,8 +1,32 @@
 import { NextApiRequest, NextApiResponse, NextPageContext } from "next";
+import { parseCookies, destroyCookie } from 'nookies'
+import { prisma } from "../prisma";
 
 export default function PrismaAdapter(
   req: NextApiRequest | NextPageContext['req'],
   res: NextApiResponse | NextPageContext['res'],
 ) {
-  return
+  return {
+    async createUser(user) {
+      const { '@bookWise:userId': userIdOnCookies } = parseCookies({ req })
+
+      if (!userIdOnCookies) {
+        throw new Error('User ID not found on cookies.')
+      }
+
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: userIdOnCookies
+        },
+
+        data: {
+          
+        }
+      })
+
+
+    }
+
+
+  }
 } 
