1f9328c4ce074adaba4ffaae32692db61fb71497
willian <williannlins@gmail.com>
2024-07-07 17:34:34 -0300

    O

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -9,33 +9,33 @@ export default function PrismaAdapter(
   res: NextApiResponse | NextPageContext['res'],
 ): Adapter {
   return {
-  //   async createUser(user) {
-  //     const { '@bookWise:userId': userIdOnCookies } = parseCookies({ req })
+    async createUser(user) {
+      const { '@bookWise:userId': userIdOnCookies } = parseCookies({ req })
 
-  //     if (!userIdOnCookies) {
-  //       throw new Error('User ID not found on cookies.')
-  //     }
+      if (!userIdOnCookies) {
+        throw new Error('User ID not found on cookies.')
+      }
 
-  //     const prismaUser = await prisma.user.update({
-  //       where: {
-  //         id: userIdOnCookies
-  //       },
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: userIdOnCookies
+        },
 
-  //       data: {
-  //         name: user.name,
-  //         // avatar_url: user.avatar_url
-  //       }
-  //     })
+        data: {
+          name: user.name,
+          // avatar_url: user.avatar_url
+        }
+      })
 
-  //     destroyCookie({res}, '@bookWise:userId',  { path: '/' } )
+      destroyCookie({res}, '@bookWise:userId',  { path: '/' } )
 
-  //     return{
-  //       id: prismaUser.id,
-  //       name: prismaUser.name,
-  //       avatar_url: prismaUser.avatar_url
-  //     }
+      return{
+        id: prismaUser.id,
+        name: prismaUser.name,
+        avatar_url: prismaUser.avatar_url
+      }
 
-  //   }
+    }
 
-  // }
+  }
 } 
